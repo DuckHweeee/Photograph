f35951ee090e150aa304453977e161564a917492
DuckHweeee <huy.mai.set18@eiu.edu.vn>
2026-09-25 10:40:10 +0700

    Initial Commit

---
+++ photograph/photographApp.swift
@@ -0,0 +1,17 @@
+//
+//  photographApp.swift
+//  photograph
+//
+//  Created by IIC4.0 on 25/9/26.
+//
+
+import SwiftUI
+
+@main
+struct photographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
